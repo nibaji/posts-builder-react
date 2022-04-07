1a56dd18699b40d2ed23d5dd0263b3309f9eb260
Nidhun Balaji T R <nidhun.balaji@cognitiveclouds.com>
2022-04-07 17:21:00 +0530

    test cases for json component

---
+++ src/__tests__/Home.test.tsx
@@ -0,0 +1,33 @@
+/** @format */
+
+import { render, screen } from "@testing-library/react";
+import Home from "../screens/Home";
+
+jest.mock("react-router-dom", () => {
+	return {
+		useNavigate: jest.fn(),
+	};
+});
+
+describe("Home", () => {
+	test("renders without crashing", () => {
+		render(<Home />);
+	});
+
+	test("matches snapshot", () => {
+		const view = render(<Home />);
+		expect(view).toMatchSnapshot();
+	});
+
+	test("contains heading", () => {
+		render(<Home />);
+		const linkElement = screen.getByText("News");
+		expect(linkElement).toBeInTheDocument();
+	});
+
+	test("has children", () => {
+		const { container } = render(<Home />);
+		const { children } = container;
+		expect(children.length).toBeGreaterThan(0);
+	});
+});
---
+++ src/__tests__/Json.test.tsx
@@ -0,0 +1,46 @@
+/** @format */
+
+import { render, screen } from "@testing-library/react";
+import Details from "../screens/Details";
+
+jest.mock("react-router-dom", () => {
+	return {
+		useLocation: () => {
+			return {
+				state: {
+					item: {
+						title: "test",
+						description: "test",
+						url: "test",
+						urlToImage: "test",
+						publishedAt: "test",
+						content: "test",
+					},
+				},
+			};
+		},
+	};
+});
+
+describe("Details", () => {
+	test("renders without crashing", () => {
+		render(<Details />);
+	});
+
+	test("matches snapshot", () => {
+		const view = render(<Details />);
+		expect(view).toMatchSnapshot();
+	});
+
+	test("contains back button", () => {
+		render(<Details />);
+		const linkElement = screen.getByText(/back/i);
+		expect(linkElement).toBeInTheDocument();
+	});
+
+	test("has children", () => {
+		const { container } = render(<Details />);
+		const { children } = container;
+		expect(children.length).toBeGreaterThan(0);
+	});
+});
